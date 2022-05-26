7d6f627f8f145af7a1823f2fc59f43716b7333bf
DerLed <48630545+DerLed@users.noreply.github.com>
2022-05-26 22:01:29 +0300

    hm7

--- Homework_7/src/main/resources/db.sql
+++ Homework_7/src/main/resources/db.sql
@@ -1,6 +1,6 @@
 DROP TABLE if EXISTS public.hall CASCADE;
 DROP table if EXISTS public.sector CASCADE;
-DROP TABLE if EXISTS public.movies CASCADE;
+DROP TABLE if EXISTS public.movie CASCADE;
 DROP TABLE if EXISTS public.place CASCADE;
 DROP TABLE if EXISTS public."row" CASCADE;
 DROP TABLE if EXISTS public."show" CASCADE;
@@ -11,17 +11,16 @@ DROP table if EXISTS public.ticket_status CASCADE;
 
 
 
-
-CREATE TABLE public.movies
+CREATE TABLE public.movie
 (
-    id          integer GENERATED ALWAYS AS identity
+    id integer GENERATED ALWAYS AS identity
         CONSTRAINT movie_pk PRIMARY KEY,
     title       varchar(255)  NOT null,
     description varchar(2000) NOT null,
     duration    integer       not null
 );
-COMMENT
-ON COLUMN public.movies.duration IS 'minute';
+
+COMMENT ON COLUMN public.movie.duration IS 'minute';
 
 CREATE TABLE public.hall
 (
@@ -40,7 +39,7 @@ CREATE TABLE public."show"
 );
 
 
-create table row
+create table public."row"
 (
     id      integer generated always as identity
         constraint row_pk primary key,
@@ -50,21 +49,25 @@ create table row
     unique (NUMBER, HALL_ID)
 );
 
-CREATE TABLE public.sector (
-	id integer NOT NULL GENERATED ALWAYS AS identity CONSTRAINT sector_pk PRIMARY KEY,
-	sector_title varchar not null
+CREATE TABLE public.sector
+(
+    id           integer NOT NULL GENERATED ALWAYS AS identity
+        CONSTRAINT sector_pk PRIMARY KEY,
+    sector_title varchar not null
 );
 
 create table public.place
 (
-    id     integer generated always as identity
+    id        integer generated always as identity
         constraint place_pk primary key,
-    number smallint Not null,
-    row_id integer  not null constraint row_fk references public.row (id),
-    sector_id integer  constraint sector_fk references public.sector (id)
+    number    smallint Not null,
+    row_id    integer  not null
+        constraint row_fk references public."row" (id),
+    sector_id integer
+        constraint sector_fk references public.sector (id)
 );
 
-create table usr
+create table public.usr
 (
     id    integer GENERATED ALWAYS AS identity
         CONSTRAINT user_pk PRIMARY KEY,
@@ -72,24 +75,28 @@ create table usr
     email varchar(255) NOT null
 );
 
-CREATE TABLE public.ticket_status (
-	id integer NOT NULL GENERATED ALWAYS AS identity CONSTRAINT ticket_status_pk PRIMARY KEY,
-	ticket_status varchar NOT NULL
+CREATE TABLE public.ticket_status
+(
+    id            integer NOT NULL GENERATED ALWAYS AS identity
+        CONSTRAINT ticket_status_pk PRIMARY KEY,
+    ticket_status varchar NOT NULL
 );
 
 
 
 CREATE TABLE public.ticket
 (
-    id       integer GENERATED ALWAYS AS identity
+    id               integer GENERATED ALWAYS AS identity
         CONSTRAINT ticket_pk PRIMARY KEY,
-    show_id  integer not null
+    show_id          integer not null
         constraint show_fk references public.show (id),
-    place_id integer not null
+    place_id         integer not null
         constraint place_fk references public.place (id),
-    user_id integer constraint user_fk references usr(id),
-    ticket_status_id integer not null constraint ticket_status_fk references ticket_status(id),
-    cost     integer not null,
+    user_id          integer
+        constraint user_fk references usr (id),
+    ticket_status_id integer not null
+        constraint ticket_status_fk references ticket_status (id),
+    cost             integer not null,
     unique (show_id, place_id)
 );
 
@@ -400,25 +407,25 @@ INSERT INTO public.usr (name, email)
 VALUES ('Евгений', '555@mail.ru');
 
 
-INSERT INTO public.movies (title, description, duration)
+INSERT INTO public.movie (title, description, duration)
 values ('Побег из Шоушенка', '1994	Фрэнк Дарабонт	драма', 142);
-INSERT INTO public.movies (title, description, duration)
+INSERT INTO public.movie (title, description, duration)
 values ('Крёстный отец', '1972	Фрэнсис Форд Коппола	детектив, драма', 175);
-INSERT INTO public.movies (title, description, duration)
+INSERT INTO public.movie (title, description, duration)
 values ('Крёстный отец 2', '1974	Фрэнсис Форд Коппола	детектив, драма', 202);
-INSERT INTO public.movies (title, description, duration)
+INSERT INTO public.movie (title, description, duration)
 values ('Тёмный рыцарь', '2008	Кристофер Нолан	боевик, детектив, драма', 152);
-INSERT INTO public.movies (title, description, duration)
+INSERT INTO public.movie (title, description, duration)
 values ('12 разгневанных мужчин', '1957	Сидни Люмет	драма, детектив', 130);
-INSERT INTO public.movies (title, description, duration)
+INSERT INTO public.movie (title, description, duration)
 values ('Список Шиндлера', '1993	Стивен Спилберг	драма, биография, исторический фильм', 130);
-INSERT INTO public.movies (title, description, duration)
+INSERT INTO public.movie (title, description, duration)
 values ('Властелин колец: Возвращение короля', '2003	Питер Джексон	фэнтези, приключение, боевик', 130);
-INSERT INTO public.movies (title, description, duration)
+INSERT INTO public.movie (title, description, duration)
 values ('Криминальное чтиво', '1994	Квентин Тарантино	чёрная комедия, драма', 90);
-INSERT INTO public.movies (title, description, duration)
+INSERT INTO public.movie (title, description, duration)
 values ('Властелин колец: Братство Кольцо', '2001	Питер Джексон	фэнтези, приключение, боевик', 90);
-INSERT INTO public.movies (title, description, duration)
+INSERT INTO public.movie (title, description, duration)
 values ('Хороший, плохой, злой', '1966	Серджо Леоне	приключение, вестерн', 90);
 
 INSERT INTO public."show" (movie_id, time_start)
@@ -443,12 +450,18 @@ INSERT INTO public."show" (movie_id, time_start)
 VALUES (4, '2022-05-22 16:00:00.000');
 
 
-INSERT INTO public.ticket_status (ticket_status) VALUES('ON_SALE');
-INSERT INTO public.ticket_status (ticket_status) VALUES('SOLD');
-INSERT INTO public.ticket_status (ticket_status) VALUES('CANCELLED');
-INSERT INTO public.ticket_status (ticket_status) VALUES('BOOKING');
-INSERT INTO public.ticket_status (ticket_status) VALUES('EXPIRED');
-INSERT INTO public.ticket_status (ticket_status) VALUES('USED');
+INSERT INTO public.ticket_status (ticket_status)
+VALUES ('ON_SALE');
+INSERT INTO public.ticket_status (ticket_status)
+VALUES ('SOLD');
+INSERT INTO public.ticket_status (ticket_status)
+VALUES ('CANCELLED');
+INSERT INTO public.ticket_status (ticket_status)
+VALUES ('BOOKING');
+INSERT INTO public.ticket_status (ticket_status)
+VALUES ('EXPIRED');
+INSERT INTO public.ticket_status (ticket_status)
+VALUES ('USED');
 
 INSERT INTO public.ticket (show_id, place_id, cost, ticket_status_id)
 VALUES (1, 2, 100, 1);
@@ -459,75 +472,127 @@ VALUES (1, 4, 100, 1);
 INSERT INTO public.ticket (show_id, place_id, cost, ticket_status_id)
 VALUES (1, 5, 100, 1);
 
-CREATE TABLE public.genres (
-	id integer NOT NULL GENERATED ALWAYS AS identity
-	constraint genres_pk primary key,
-	"name" varchar(64) not NULL
+CREATE TABLE public.genre
+(
+    id     integer     NOT NULL GENERATED ALWAYS AS identity
+        constraint genre_pk primary key,
+    "name" varchar(64) not NULL
 );
 
-create table genres_movies(
-	movies_id integer not null constraint movies_fk references movies (id),
-	genres_id integer not null constraint genres_fk references genres (id),
-	primary key (movies_id, genres_id)
+create table public.genre_movie
+(
+    movie_id integer not null
+        constraint movie_fk references movie (id),
+    genre_id integer not null
+        constraint genre_fk references genre (id),
+    primary key (movie_id, genre_id)
 );
 
 
-Insert into genres (name) VALUES ('аниме');
-Insert into genres (name) VALUES ('биографический');
-Insert into genres (name) VALUES ('боевик');
-Insert into genres (name) VALUES ('вестерн');
-Insert into genres (name) VALUES ('военный');
-Insert into genres (name) VALUES ('детектив');
-Insert into genres (name) VALUES ('детский');
-Insert into genres (name) VALUES ('документальный');
-Insert into genres (name) VALUES ('драма');
-Insert into genres (name) VALUES ('исторический');
-Insert into genres (name) VALUES ('комедия');
-Insert into genres (name) VALUES ('концерт');
-Insert into genres (name) VALUES ('короткометражный');
-Insert into genres (name) VALUES ('криминал');
-Insert into genres (name) VALUES ('мелодрама');
-Insert into genres (name) VALUES ('мистика');
-Insert into genres (name) VALUES ('музыка');
-Insert into genres (name) VALUES ('мультфильм');
-Insert into genres (name) VALUES ('мюзикл');
-Insert into genres (name) VALUES ('научный');
-Insert into genres (name) VALUES ('нуар');
-Insert into genres (name) VALUES ('приключения');
-Insert into genres (name) VALUES ('реалити-шоу');
-Insert into genres (name) VALUES ('семейный');
-Insert into genres (name) VALUES ('спорт');
-Insert into genres (name) VALUES ('ток-шоу');
-Insert into genres (name) VALUES ('триллер');
-Insert into genres (name) VALUES ('ужасы');
-Insert into genres (name) VALUES ('фантастика');
-Insert into genres (name) VALUES ('фэнтези');
+Insert into public.genre (name) VALUES ('аниме');
+Insert into public.genre (name) VALUES ('биографический');
+Insert into public.genre (name)
+VALUES ('боевик');
+Insert into public.genre (name)
+VALUES ('вестерн');
+Insert into public.genre (name)
+VALUES ('военный');
+Insert into public.genre (name)
+VALUES ('детектив');
+Insert into public.genre (name)
+VALUES ('детский');
+Insert into public.genre (name)
+VALUES ('документальный');
+Insert into public.genre (name)
+VALUES ('драма');
+Insert into public.genre (name)
+VALUES ('исторический');
+Insert into public.genre (name)
+VALUES ('комедия');
+Insert into public.genre (name)
+VALUES ('концерт');
+Insert into public.genre (name)
+VALUES ('короткометражный');
+Insert into public.genre (name)
+VALUES ('криминал');
+Insert into public.genre (name)
+VALUES ('мелодрама');
+Insert into public.genre (name)
+VALUES ('мистика');
+Insert into public.genre (name)
+VALUES ('музыка');
+Insert into public.genre (name)
+VALUES ('мультфильм');
+Insert into public.genre (name)
+VALUES ('мюзикл');
+Insert into public.genre (name)
+VALUES ('научный');
+Insert into public.genre (name)
+VALUES ('нуар');
+Insert into public.genre (name)
+VALUES ('приключения');
+Insert into public.genre (name)
+VALUES ('реалити-шоу');
+Insert into public.genre (name)
+VALUES ('семейный');
+Insert into public.genre (name)
+VALUES ('спорт');
+Insert into public.genre (name)
+VALUES ('ток-шоу');
+Insert into public.genre (name)
+VALUES ('триллер');
+Insert into public.genre (name)
+VALUES ('ужасы');
+Insert into public.genre (name)
+VALUES ('фантастика');
+Insert into public.genre (name)
+VALUES ('фэнтези');
 
 
-INSERT INTO public.genres_movies (movies_id, genres_id) VALUES(1, 9);
-INSERT INTO public.genres_movies (movies_id, genres_id) VALUES(2, 9);
-INSERT INTO public.genres_movies (movies_id, genres_id) VALUES(2, 6);
-INSERT INTO public.genres_movies (movies_id, genres_id) VALUES(3, 6);
-INSERT INTO public.genres_movies (movies_id, genres_id) VALUES(3, 9);
-INSERT INTO public.genres_movies (movies_id, genres_id) VALUES(4, 9);
-INSERT INTO public.genres_movies (movies_id, genres_id) VALUES(6, 9);
-INSERT INTO public.genres_movies (movies_id, genres_id) VALUES(6, 2);
-INSERT INTO public.genres_movies (movies_id, genres_id) VALUES(6, 10);
-INSERT INTO public.genres_movies (movies_id, genres_id) VALUES(6, 6);
-INSERT INTO public.genres_movies (movies_id, genres_id) VALUES(5, 9);
-INSERT INTO public.genres_movies (movies_id, genres_id) VALUES(7, 29);
-INSERT INTO public.genres_movies (movies_id, genres_id) VALUES(7, 21);
-INSERT INTO public.genres_movies (movies_id, genres_id) VALUES(7, 3);
-INSERT INTO public.genres_movies (movies_id, genres_id) VALUES(8, 9);
-INSERT INTO public.genres_movies (movies_id, genres_id) VALUES(8, 11);
-INSERT INTO public.genres_movies (movies_id, genres_id) VALUES(9, 3);
-INSERT INTO public.genres_movies (movies_id, genres_id) VALUES(9, 29);
-INSERT INTO public.genres_movies (movies_id, genres_id) VALUES(9, 21);
-INSERT INTO public.genres_movies (movies_id, genres_id) VALUES(10, 21);
-INSERT INTO public.genres_movies (movies_id, genres_id) VALUES(10, 4);
-INSERT INTO public.genres_movies (movies_id, genres_id) VALUES(11, 3);
-INSERT INTO public.genres_movies (movies_id, genres_id) VALUES(11, 16);
-INSERT INTO public.genres_movies (movies_id, genres_id) VALUES(11, 28);
+INSERT INTO public.genre_movie (movie_id, genre_id)
+VALUES (1, 9);
+INSERT INTO public.genre_movie (movie_id, genre_id)
+VALUES (2, 9);
+INSERT INTO public.genre_movie (movie_id, genre_id)
+VALUES (2, 6);
+INSERT INTO public.genre_movie (movie_id, genre_id)
+VALUES (3, 6);
+INSERT INTO public.genre_movie (movie_id, genre_id)
+VALUES (3, 9);
+INSERT INTO public.genre_movie (movie_id, genre_id)
+VALUES (4, 9);
+INSERT INTO public.genre_movie (movie_id, genre_id)
+VALUES (6, 9);
+INSERT INTO public.genre_movie (movie_id, genre_id)
+VALUES (6, 2);
+INSERT INTO public.genre_movie (movie_id, genre_id)
+VALUES (6, 10);
+INSERT INTO public.genre_movie (movie_id, genre_id)
+VALUES (6, 6);
+INSERT INTO public.genre_movie (movie_id, genre_id)
+VALUES (5, 9);
+INSERT INTO public.genre_movie (movie_id, genre_id)
+VALUES (7, 29);
+INSERT INTO public.genre_movie (movie_id, genre_id)
+VALUES (7, 21);
+INSERT INTO public.genre_movie (movie_id, genre_id)
+VALUES (7, 3);
+INSERT INTO public.genre_movie (movie_id, genre_id)
+VALUES (8, 9);
+INSERT INTO public.genre_movie (movie_id, genre_id)
+VALUES (8, 11);
+INSERT INTO public.genre_movie (movie_id, genre_id)
+VALUES (9, 3);
+INSERT INTO public.genre_movie (movie_id, genre_id)
+VALUES (9, 29);
+INSERT INTO public.genre_movie (movie_id, genre_id)
+VALUES (9, 21);
+INSERT INTO public.genre_movie (movie_id, genre_id)
+VALUES (10, 21);
+INSERT INTO public.genre_movie (movie_id, genre_id)
+VALUES (10, 4);
+
+
 --INSERT INTO public.user_ticket (user_id, ticket_id)
 --VALUES (1, 3);
 --INSERT INTO public.user_ticket (user_id, ticket_id)
